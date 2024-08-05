f3b9469c9c28b65930b61fa532b4516f91fbdde3
Rperaltaflp <rperalta@flp.uy>
2024-08-05 10:32:03 -0300

    Git ignore

--- SistemaIntegralReportes/obj/Release/net6.0/SistemaIntegralReportes.AssemblyInfo.cs
+++ SistemaIntegralReportes/obj/Release/net6.0/SistemaIntegralReportes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaIntegralReportes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34e73f454c9a0da1011cc82f28a0c74b95c46b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ed58f89340d810a99e7e59f58817c7846cb4eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaIntegralReportes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaIntegralReportes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
