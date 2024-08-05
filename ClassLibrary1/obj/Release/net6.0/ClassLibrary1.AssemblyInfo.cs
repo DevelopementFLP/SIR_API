b34e73f454c9a0da1011cc82f28a0c74b95c46b6
Rperaltaflp <rperalta@flp.uy>
2024-08-05 10:11:44 -0300

    Prueba de Proyecto

--- ClassLibrary1/obj/Release/net6.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Release/net6.0/ClassLibrary1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SistemaIntegralReportes")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06a772a89ccb495bf21633c6333322f2085c0f5")]
-[assembly: System.Reflection.AssemblyProductAttribute("SistemaIntegralReportes")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SistemaIntegralReportes")]
+[assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
