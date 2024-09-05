f7651a49fb6e899e9acb4e1fd550cdce4fd20936
RperaltaFLP <rperalta@flp.uy>
2024-09-05 07:33:44 -0300

    c

---
+++ SistemaIntegralReportes/Models/CodigoFechaPrecios.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaIntegralReportes.Models
+{
+    public class CodigoFechaPrecios
+    {
+        public string Codigo {  get; set; }
+        public DateTime FechaProduccion { get; set; }
+        public double Precio { get; set; }
+    }
+}
