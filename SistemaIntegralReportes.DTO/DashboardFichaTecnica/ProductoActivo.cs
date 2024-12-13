97e04decc7e132403bf5aeb39b9f1fbe4f6a6213
GonzaloSilvera21 <gsilveradesarrollos@gmail.com>
2024-12-13 12:17:37 -0300

    Dashboard ficha técnica

---
+++ SistemaIntegralReportes.DTO/DashboardFichaTecnica/ProductoActivo.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaIntegralReportes.DTO.DashboardFichaTecnica
+{
+    public class ProductoActivo
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SistemaIntegralReportes/Controllers/DashboardFichaTecnica/DashboardFichaTecnicaController.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SistemaIntegralReportes.DTO.DashboardFichaTecnica;
+using System.Data;
+
+namespace SistemaIntegralReportes.Controllers.DashboardFichaTecnica
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardFichaTecnicaController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _innovaConnectionString;
+
+        public DashboardFichaTecnicaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _innovaConnectionString = configuration.GetConnectionString("InnovaProduccion");
+        }
+
+        [HttpGet("GetProductosActivosAsync")]
+        public async Task<IEnumerable<ProductoActivo>> GetProductosActivosAsync()
+        {
+            var query = _configuration.GetSection("DashboardFichaTecnica:GetProductosOrdenesActivas").Value.ToString();
+            if (query == null) return Enumerable.Empty<ProductoActivo>();
+
+            using (var connection = new SqlConnection(_innovaConnectionString))
+            {
+                if (connection == null) return Enumerable.Empty<ProductoActivo>();
+
+                try
+                {
+                    await connection.OpenAsync();
+                    var productosActivos = await connection.QueryAsync<ProductoActivo>(query, commandType:CommandType.Text);
+                    return productosActivos;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Error al ejecutar la consulta: " + e.Message);
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+        }
+
+        [HttpGet("GetTiempoProductoActivo")]
+        public int GetTiempoProductoActivo()
+        {
+            int defaultTime = 5000;
+
+            var time = _configuration.GetSection("DashboardFichaTecnica:TiempoProductoActivo").Value.ToString();
+            
+            if (time == null) return defaultTime;
+
+            return int.Parse(time);
+            
+        }
+
+        [HttpGet("GetTiempoActualizacionDashboard")]
+        public int GetTiempoActualizacionDashboard()
+        {
+            int defaultTime = 300000;
+            var time = _configuration.GetSection("DashboardFichaTecnica:TiempoActualizacionDashboard").Value.ToString();
+            if (time == null) return defaultTime; 
+            return int.Parse(time);
+        }
+    }
+}
